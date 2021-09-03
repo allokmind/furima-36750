1b325141472dfc0b04c466f9a7c98f02113339f5
allokmind <exapicoallok@icloud.com>
2021-09-03 23:20:14 +0900

    バリデーション設定変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,14 @@ class User < ApplicationRecord
   end
 
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' } do
-  validates :last_name_kana
-  validates :first_name_kana
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  with_options format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください' } do
+    validates :password
   end
 
   validates :birth, presence: true
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end

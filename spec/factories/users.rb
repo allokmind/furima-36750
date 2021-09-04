71052666e3fa5f50d047d11a06fb63deb3d38788
allokmind <exapicoallok@icloud.com>
2021-09-04 13:34:27 +0900

    Rubocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.name}
-    email {Faker::Internet.free_email}
+    nickname { Faker::Name.name }
+    email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
+    password { password }
+    password_confirmation { password }
 
     transient do
       person { Gimei.name }
@@ -13,7 +13,7 @@ FactoryBot.define do
     last_name { person.last.kanji }
     first_name_kana { person.first.katakana }
     last_name_kana { person.last.katakana }
-    
+
     birth { Faker::Date.backward }
   end
 end

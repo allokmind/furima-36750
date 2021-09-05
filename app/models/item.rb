426912deb80a2302d3980cd6f6c2d41f5d188bad
allokmind <exapicoallok@icloud.com>
2021-09-05 19:01:01 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,12 @@ class Item < ApplicationRecord
   belongs_to :shipping_cost
   belongs_to :prefecture
   belongs_to :shipping_day
+
+  validates :item_name, :description, :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" } 
 end

e35f8054fbca228443a5f8ac56fadeee77130d99
allokmind <exapicoallok@icloud.com>
2021-09-06 08:45:44 +0900

    価格へのバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,8 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
-  validates :item_name, :description, :price, presence: true
+  validates :item_name, :description, presence: true
+  validates :price, presence: true, numericality: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' }
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 

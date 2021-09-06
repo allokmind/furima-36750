f4fcf26141da14abc45aaab3c98c0306d4cefe1f
allokmind <exapicoallok@icloud.com>
2021-09-06 10:49:13 +0900

    販売手数料・利益計算結果 追加

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,13 @@
-window.addEventListener('load', () => {
-  console.log("OK");
-});
+function itemPrice() {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue*0.1)
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = inputValue - Math.floor(inputValue*0.1)
+ })
+};
+
+window.addEventListener('load', itemPrice)

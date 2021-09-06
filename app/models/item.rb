2d8eeebd459b11661566dfeff0ba2339d193467e
allokmind <exapicoallok@icloud.com>
2021-09-06 14:59:02 +0900

    Rubocop実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,12 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
 
   validates :item_name, :description, :image, presence: true
-  validates :price, presence: true, numericality: { with: /\A[0-9]+\z/, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, presence: true,
+                    numericality: { with: /\A[0-9]+\z/, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
 end

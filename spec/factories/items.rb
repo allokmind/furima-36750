c3ab7d283a24a6d115bc395663b063c396524443
allokmind <exapicoallok@icloud.com>
2021-09-06 18:10:24 +0900

    Faker設定値ミス修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     description       { Faker::Lorem.sentence }
     category_id       { Faker::Number.within(range: 2..11) }
     status_id         { Faker::Number.within(range: 2..7) }
-    shipping_cost_id  { Faker::Number.within(range: 2..11) }
-    prefecture_id     { Faker::Number.within(range: 2..3) }
+    shipping_cost_id  { Faker::Number.within(range: 2..3) }
+    prefecture_id     { Faker::Number.within(range: 2..48) }
     shipping_day_id   { Faker::Number.within(range: 2..4) }
     price             { Faker::Number.within(range: 300..9_999_999) }
     association :user

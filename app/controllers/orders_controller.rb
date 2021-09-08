1404c19decf59410035f3b2dc650dbe836d94c22
allokmind <exapicoallok@icloud.com>
2021-09-08 14:22:30 +0900

    createアクション定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,29 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_params, only: [:index, :create]
+
   def index
     @order_address = OrderAddress.new
-    @item = Item.find(params[:item_id])
   end
   
+  def create
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid? 
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_params
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_address_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address_line, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end

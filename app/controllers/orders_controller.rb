47c15f6d8458ddb833077fa52ebca705e573f180
allokmind <exapicoallok@icloud.com>
2021-09-08 12:23:59 +0900

    購入商品情報を渡す変数定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   def index
     @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
   end
   
 end

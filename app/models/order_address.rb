0222249fbbe76f6e7194db166ba7f48384b4c2e7
allokmind <exapicoallok@icloud.com>
2021-09-08 14:21:50 +0900

    バリデーション・saveメソッド変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -10,12 +10,11 @@ class OrderAddress
     validates :city
     validates :address_line
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Exclude hyphen(-)"}
-    validates :order_id
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
      order = Order.create(user_id: user_id, item_id: item_id)
-     Address.create(postal_code: postal_code, prefecture_id: prefecture.id, city: city, address_line: address_line, building_name: building_name, phone_number: phone_number, order_id: order.id)
+     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address_line: address_line, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end

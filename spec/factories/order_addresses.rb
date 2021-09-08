fbbff3dbdec7930c29cb7842f7aadafd9c9c22f4
allokmind <exapicoallok@icloud.com>
2021-09-08 15:47:29 +0900

    FactoryBot生成

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :order_address do
-    
+    postal_code       { '123-4567' }
+    prefecture_id     { Faker::Number.within(range: 2..48) }
+    city              { Faker::Address.city }
+    address_line      { Faker::Address.street_address }
+    building_name     { Faker::Address.building_number }
+    phone_number      { Faker::Number.leading_zero_number(digits: 10) }
   end
 end

293481c6e8ea35e8abce9621795726a6af6a1579
allokmind <exapicoallok@icloud.com>
2021-09-09 17:20:21 +0900

    エラーメッセージ変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,7 +9,7 @@ class OrderAddress
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city
     validates :address_line
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Exclude hyphen(-)' }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid.' }
     validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
